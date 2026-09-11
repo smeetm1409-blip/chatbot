98796bac003a196d284e583a76c9318b9803b609
smeetm1409-blip <smeetm1409@gmail.com>
2026-09-11 19:36:28 +0530

    Update backend.py
    changed error

--- backend.py
+++ backend.py
@@ -9,7 +9,7 @@ from google import genai
 
 load_dotenv()
 
-API_KEY = os.getenv("GEMINI_API_KEY")
+API_KEY = os.getenv("GEMINI_API_KEY","AQ.Ab8RN6KCTXE1mCUTXDseuHfFosQGdOXIFLf1C7_E6BqIpksuyg")
 
 if not API_KEY:
     raise ValueError(
